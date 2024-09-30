0c51d6542fbcec48539b6df55687ce76c415e84d
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-09-30 14:17:02 +0800

    OstonalGeloOutput.java
    The OstonalGeloOutput class implements an application that simply prints "Kamusta Mundo" to standard output.

---
+++ OstonalGeloOutput.java
@@ -0,0 +1,8 @@
+// The OstonalGeloOutput class implements an application that
+//simply prints "Kamusta Mundo" to standard output.
+   
+class OstonalGeloOutput {                             // Define the class OstonalGeloOutput
+    public static void main(String[] args) {          // Main method where program execution begins
+        System.out.println("Kamusta Mundo!");         // Display the string "Kamusta Mundo!"
+    }                                                 // End of main method
+}                                                     // End of OstonalGeloOutput class
