9412a50b2db2bbbfe197f29749b3d15ac99b4527
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-10-08 16:30:33 +0800

    Create Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,16 @@
+public class Primitive {
+    public static void main(String[] args) {
+      
+        // Primitive data types
+        byte b = 0;
+        char c = 'H';
+        int i = 3110;
+        short t = 1;
+        float f = 2.0f;
+        boolean bool = true;
+
+        String output = "" + c + i + " " + "w" + b + "r" + t + "d " + f + " " + bool;
+      
+        System.out.println(output);
+    }
+}
