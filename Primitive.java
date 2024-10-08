44eb09a3d94cd93738e183c1d1a816e6ca27dd68
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-10-08 16:16:24 +0800

    Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,16 @@
+public class Primitive {
+    public static void main(String[] args) {
+      
+        // Primitive data types
+        byte b = 0;
+        char c = 'H';
+        int i = 3110;
+        short s = 2;
+        float f = 2.0f;
+        boolean bool = true;
+
+        String output = "" + c + i + " " + "w" + b + "r" + i + "d " + f + " " + bool;
+      
+        System.out.println(output);
+    }
+}
