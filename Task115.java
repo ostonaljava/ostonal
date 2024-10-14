f83be54a895f0a59773e44ef92468e73aa157291
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-10-14 16:28:13 +0800

    Create Task115.java

---
+++ Task115.java
@@ -0,0 +1,15 @@
+public class Task115 {
+    public static void main(String[] args) {
+        String a = "Wow";                        // Change from String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = "Wow!";                      // Change from String d = c; to make the line 9 true.
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals("Pogi ko");     // Change from boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
