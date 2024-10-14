2511507e6e8efdd3101504157cbe9efff12643b6
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-10-14 16:26:46 +0800

    Update Task115.java

--- Task115.java
+++ Task115.java
@@ -1,13 +1,13 @@
 public class Task115 {
     public static void main(String[] args) {
-        String a = "Wow";                         // Change from String a = new String("Wow");
+        String a = "Wow";                        // Change from String a = new String("Wow");
         String b = "Wow";
         String c = a;
-        String d = "Wow!";                        // Change from String d = c; to make the line 9 true.
+        String d = "Wow!";                      // Change from String d = c; to make the line 9 true.
 
         boolean b1 = a == b;
         boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals("SomethingElse");  // Change from boolean b3 = !c.equals(a);
+        boolean b3 = !c.equals("Pogi ko");     // Change from boolean b3 = !c.equals(a);
 
         if (b1 && b2 && b3) {
             System.out.println("Success!");
