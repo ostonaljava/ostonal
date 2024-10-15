e71b88a3053be9e58885801e7d4964f964254a62
ostonaljava <jb6.gostonal2024@nrgitii.edu.ph>
2024-10-15 17:02:46 +0800

    Tasksheet116.java

--- Tasksheet116.java
+++ Tasksheet116.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Tasksheet116 {
+  
+    public static void main(String[] args) {
+
+       Scanner scan = new Scanner(System.in);
+       String name = scan.nextLine();
+
+       StringBuilder rev = new StringBuilder(name).reverse();
+       String newName = rev.toString();
+
+       if (name.equals(newName)) {
+            
+        System.out.println("The input string is a palindrome.");
+
+       } else {
+        
+        System.out.println("The input string is not a palindrome.");
+       
+    }
+}
+}
